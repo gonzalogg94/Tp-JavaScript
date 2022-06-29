b344ca7829e1f2ddf00820934c5abdc551f6e8f3
Gonzalo Gallardo <gonzalo94.jobs@gmail.com>
2022-06-29 12:11:15 -0300

    realice cambios en el ultimo ejercicio para que este mas completo

--- js/ejercicio5.js
+++ js/ejercicio5.js
@@ -1,4 +1,4 @@
 // Escribe un programa de tres líneas que pida un número, pida otro número y escriba el resultado de sumar estos dos números.
 numero1=Number(prompt("ingrese un numero"));
 numero2=Number(prompt("ingrese otro numero"));
-document.write(numero1+numero2);
+document.write(("El resultado de la suma de los dos numero es: ")+(numero1+numero2));
