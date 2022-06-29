a9f50d27cf6325f6cb3cf739824420afd6aae5dd
Gonzalo Gallardo <gonzalo94.jobs@gmail.com>
2022-06-28 23:14:27 -0300

    ejercicios casi resueltos

---
+++ js/ejercicio5.js
@@ -0,0 +1,3 @@
+// Escribe un programa de tres líneas que pida un número, pida otro número y escriba el resultado de sumar estos dos números.
+numero1=prompt("ingrese un numero")
+numero2=prompt("ingrese otro numero")
