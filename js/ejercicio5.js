ae385588fd9941662e9f1bc8d1821bac5b2e1981
Gonzalo Gallardo <gonzalo94.jobs@gmail.com>
2022-06-29 10:16:25 -0300

    Primeros cinco ejerccios resueltos

--- js/ejercicio5.js
+++ js/ejercicio5.js
@@ -1,3 +1,4 @@
 // Escribe un programa de tres líneas que pida un número, pida otro número y escriba el resultado de sumar estos dos números.
-numero1=prompt("ingrese un numero")
-numero2=prompt("ingrese otro numero")
+numero1=Number(prompt("ingrese un numero"));
+numero2=Number(prompt("ingrese otro numero"));
+document.write(numero1+numero2);
