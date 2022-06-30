17210496cf21dbcfd7bf45176a20f10a16da3bca
Gonzalo Gallardo <gonzalo94.jobs@gmail.com>
2022-06-30 17:48:47 -0300

    actualizacion de la clase antes del break

--- js/ejercicio2.js
+++ js/ejercicio2.js
@@ -1,3 +1,13 @@
 //  Escribe un programa de una sola línea que escriba en la pantalla un texto que diga «Hello World» (document.write).
 
 document.write("Hello World");
+
+// ejemplo de constantes
+
+const url = "<br> https://campus.rollingcodeschool.com/login/?lang=es"
+document.write(url)
+
+nombre="mauricio"
+console.log(URL)
+
+
