82de05d96578716ac255dc4732330cafd521f874
Gonzalo Gallardo <gonzalo94.jobs@gmail.com>
2022-06-30 17:31:27 -0300

    terminamos de revisar los ajercicios del tp1

--- js/ejercicio4.js
+++ js/ejercicio4.js
@@ -1,3 +1,3 @@
 // Escribe un programa de dos líneas que pida el nombre del usuario con un prompt y escriba un texto que diga «Hola nombreUsuario»
-nombreDeUsuario=prompt("Escriba su nombre")
-document.write("hola "+ (nombreDeUsuario))
+nombreDeUsuario=prompt("Escriba su nombre");
+document.write("hola "+ (nombreDeUsuario));
--- js/ejercicio5.js
+++ js/ejercicio5.js
@@ -1,4 +1,4 @@
 // Escribe un programa de tres líneas que pida un número, pida otro número y escriba el resultado de sumar estos dos números.
-numero1=Number(prompt("ingrese un numero"));
-numero2=Number(prompt("ingrese otro numero"));
-document.write(("El resultado de la suma de los dos numero es: ")+(numero1+numero2));
+let numeroUno=Number(prompt("ingrese un numero"));
+let numeroDos=Number(prompt("ingrese otro numero"));
+document.write(("El resultado de la suma de los dos numeros es: ")+(numeroUno+numeroDos));
